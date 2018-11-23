13b65d69d26226e2c7863b6c1b8d9dcb8f3bdd70
akshaykokane <30575487+akshaykokane@users.noreply.github.com>
2018-11-23 18:03:53 -0500

    Basic model completed!

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ import numpy as np
 import tensorflow as tf
 import cv2
 import glob
+import sys
 from google.protobuf.json_format import MessageToJson
 tf.logging.set_verbosity(tf.logging.INFO)
 sess = tf.Session()
@@ -61,23 +62,97 @@ def val_input_fn():
     return input_fn(filenames=["val.tfrecords"], train=False)
 
 
-## ------------------------------
-## Function to print images start
-## ------------------------------
-features, labels = train_input_fn()
-# Initialize `iterator` with training data.
-#sess.run(train_iterator.initializer)
-# Initialize `iterator` with validation data.
-#sess.run(val_iterator.initializer)
-img, label = sess.run([features['image'], labels])
-print(img.shape, label.shape)
- # Loop over each example in batch
-for i in range(img.shape[0]):
-    cv2.imshow('image', img[i])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    print('Class label ' + str(np.argmax(label[i])))
-## ------------------------------
-## Function to print images end
-## ------------------------------
-print("Done!!!!!!!!!!!!!!")
+def cnn_model_fn(features, labels, mode, params):
+    input_layer = tf.reshape(features["image"], [-1, 227, 227, 3])
+    input_layer = tf.identity(input_layer, name="input_tensor")
+    #Layer 1
+    conv1 = tf.layers.conv2d(
+        inputs=input_layer,
+        filters=96,
+        kernel_size=[7, 7],
+        padding="same",
+        activation=tf.nn.relu)
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[3, 3], strides=2)
+
+    # Layer 2
+    conv2 = tf.layers.conv2d(
+        inputs=pool1,
+        filters=256,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[3, 3], strides=2)
+
+    # Layer 3
+    conv3 = tf.layers.conv2d(
+        inputs=pool2,
+        filters=384,
+        kernel_size=[3, 3],
+        padding="same",
+        activation=tf.nn.relu)
+    pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[3, 3], strides=2)
+
+    pool3_flat=tf.reshape(pool2, [-1, 14 * 14 * 256])
+
+    # Dense Layer - 1
+    dense1 = tf.layers.dense(inputs=pool3_flat, units=512, activation=tf.nn.relu)
+
+    # DropOut - 1
+    dropout1 = tf.layers.dropout(
+        inputs=dense1, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+
+
+    # Logits layer
+    # Input Tensor Shape: [batch_size, 1024]
+    # Output Tensor Shape: [batch_size, 10]
+    logits = tf.layers.dense(inputs=dropout1, units=8)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])}
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+# Create the Estimator
+age_classifier = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="/tmp/age_convnet_model")
+
+
+# Set up logging for predictions
+# Log the values in the "Softmax" tensor with label "probabilities"
+tensors_to_log = {"probabilities": "softmax_tensor"}
+logging_hook = tf.train.LoggingTensorHook(
+    tensors=tensors_to_log, every_n_iter=50)
+
+count = 0
+while (count < 1):
+    age_classifier.train(input_fn=train_input_fn, steps=1000)
+    result = age_classifier.evaluate(input_fn=val_input_fn)
+    print(result)
+    print("Classification accuracy: {0:.2%}".format(result["accuracy"]))
+    sys.stdout.flush()
+    count = count + 1
